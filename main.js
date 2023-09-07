c669efcf2ae3affcb7d34f3fd1637d75cd8dff2e
UtkarshPriy <100868987+UtkarshPriy@users.noreply.github.com>
2023-09-07 23:34:34 +0530

    added other pages

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
-var MD5 = require("crypto-js/md5"); 
-console.log(MD5("text to hash").toString());
+
+let name_input = document.querySelector('#name_input');
+let searchBtn = document.querySelector('#search_btn');
+let superhero_list = document.querySelector('#superhero_list');
+let superhero_detail = document.querySelector("#superhero_detail");
+let fav_detail = document.querySelector("#fav_detail");
+let heroes =[];
+
+function fetch_hero(){
+    let hero = e.
+
+}
+
+function display_hero(){
+
+}
+
+function search(){
+
+}
+
+function fav_handler(){
+
+}
+
+function detailed_info(){
+
+}
+
+function show_fav(){
+
+}
+
+searchBtn.addEventListener('click',fetch_hero())
