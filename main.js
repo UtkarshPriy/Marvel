ef49a8f1fc26237d3ef3f05bb1add620c4d6eaaf
UtkarshPriy <100868987+UtkarshPriy@users.noreply.github.com>
2023-09-08 13:34:03 +0530

    functions

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 let name_input = document.querySelector('#name_input');
 let searchBtn = document.querySelector('#search_btn');
-// let superhero_list = document.querySelector('#superhero_list');
+let errordiv = document.querySelector('#errormsg');
+let superhero_list = document.querySelector('#superhero_list');
 // let superhero_detail = document.querySelector("#superhero_detail");
 // let fav_detail = document.querySelector("#fav_detail");
 let heroes =[];
@@ -11,18 +12,48 @@ const privateKey = '1db1bb50d8bb4dab5cb37d60468465a6caeea05f';
 // let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=ac8d9167414964ceca0043193cc9161b&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`;
 // https://gateway.marvel.com:443/v1/public/characters?ts=<time-stamp>&apikey=<public-key>&hash=<md5(ts+privateKey+publicKey)>
 
-let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`;
 
-function fetch_hero(){
-    let hero = e.
+// function fetch_hero(){
+//     let hero = e.;
+
+// }
+function errormessage(text){
+    errordiv.innerHTML = text;
+
+
+}
+function addHerotoDom(hero){
+    let li = document.createElement('li');
+    li.innerHTML =`
+    <img src="${hero.thumbnail.path+"."+hero.thumbnail.extension}" id = "poster"  >
+    <h2 id="heroTitle"  data-id=${hero.id}>${hero.name}</h2>
+    <button id="details" data-id="${hero.id}"> details </button>
+    <button id="favBtn" data-id="${hero.id}" data-title="${hero.name}" >Add to Favourites</button>
+    `
+    heroes.append(li);
+}
+
+function renderList(){
+    if(hero.length == 0){
+        let errormsg = 'Please input some valid hero name'
+
+    }
+    if(hero.length>0){
+        for(i =0 ;i<hero.length;i++){
+            addHerotoDom(heroes[i]);
+        }
+    }
 
 }
 
-function display_hero(){
+async function search(text){
+    let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`;
+    let response = await fetch(url);
+    let data1 = await response.json();
+    hero = await data1.data.results;
+    renderList();
 
-}
 
-function search(){
 
 }
 
@@ -37,17 +68,20 @@ function detailed_info(){
 function show_fav(){
 
 }
-handleInput(){
+
+function handleInput(data){
+    let text = data.value;
+    search(text);
 
 }
 
 function handleKeyandClick(e){
-    if(e.target.id = 'inputBar'){
+    if(e.target.id == 'inputBar'){
         handleInput(e.target);
 
     }
 
 }
 
-searchBtn.addEventListener('click',handleKeyandClick());
-name_input.addEventListener('keyup',handleKeyandClick())
+searchBtn.addEventListener('click',handleKeyandClick);
+name_input.addEventListener('keyup',handleKeyandClick);
