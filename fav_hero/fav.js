d8aa4aa9007fff555fddf272daca00f76e92f56f
UtkarshPriy <100868987+UtkarshPriy@users.noreply.github.com>
2023-09-09 12:25:14 +0530

    feat - deletion

---
+++ fav_hero/fav.js
@@ -0,0 +1,78 @@
+    let favListInDom = document.querySelector('#favList');
+    let favList = JSON.parse(localStorage.getItem('favHeros'));
+    console.log(favList);
+
+    function addFavToDom(heroDetails) {
+        let li = document.createElement('li');
+        li.innerHTML =
+            `
+        <img src="${heroDetails.thumbnail.path+"."+heroDetails.thumbnail.extension}" id = "poster"  >
+        <h2 id="heroTitle"  data-id=${heroDetails.id}>${heroDetails.name}</h2>
+        <button id="details" data-id="${heroDetails.id}"> details </button>
+        <button id="delete" data-id="${heroDetails.id}"> delete </button>
+        `;
+        favListInDom.append(li);
+
+
+    }
+
+    // function deleteHero(e){
+
+    //     let newList = favList.filter((id)=>{
+    //         return favList.id != e.target.value.dataset.id
+    //     })
+
+    // }
+
+    async function renderList() {
+        favListInDom.innerHTML = "";
+        let heroDetails = [];
+        console.log('test');
+        if (favList === 0) {
+            return
+        }
+        if (favList.length != 0) {
+
+            for (i = 0; i < favList.length; i++) {
+                let id = favList[i];
+                let response = await fetch(`https://gateway.marvel.com/v1/public/characters/${id}?apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`);
+                let heroDetails = (await response.json()).data.results[0];
+
+                addFavToDom(heroDetails);
+
+            }
+        }
+
+
+
+    }
+
+    function handleKeyandClick(e) {
+        console.log('favList');
+        if (e.target.value.id = 'delete') {
+
+
+            let newList = [];
+            for (i of favList) {
+                if (i != e.target.dataset.id) {
+                    newList.push(i);
+                }
+            }
+            // let newList =  favList.filter((id)=>{
+            //     return favList[i] !== e.target.dataset.id;
+            // })
+            favList = [...newList];
+            console.log(favList);
+            localStorage.setItem("favHeros", JSON.stringify(favList));
+            let ele = document.getElementById(e.target.dataset.id);
+            e.target.parentElement.style.display = "none";
+            renderList();
+
+        }
+
+
+    }
+
+
+    document.addEventListener('click', handleKeyandClick);
+    renderList();
--- main.js
+++ main.js
@@ -1,35 +1,22 @@
-
 let name_input = document.querySelector('#name_input');
 let searchBtn = document.querySelector('#search_btn');
 let errordiv = document.querySelector('#errormsg');
+console.log(errordiv);
 let superhero_list = document.querySelector('#superhero_list');
 let heroes = [];
 let fav = [];
 
-// let inputBar =document.querySelector('#name_input');
-// let superhero_detail = document.querySelector("#superhero_detail");
-// let fav_detail = document.querySelector("#fav_detail");
 
-// const publicKey = 'ac8d9167414964ceca0043193cc9161b';
-// const privateKey = '1db1bb50d8bb4dab5cb37d60468465a6caeea05f';
-// // let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=ac8d9167414964ceca0043193cc9161b&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`;
-// // https://gateway.marvel.com:443/v1/public/characters?ts=<time-stamp>&apikey=<public-key>&hash=<md5(ts+privateKey+publicKey)>
-
-
-// // function fetch_hero(){
-// //     let hero = e.;
-
-// // }
-// function errormessage(text){
-
-//     errordiv.innerHTML = '<h3>text</h3>';
-
-
+// function errormessage(message) {
+//     let innerElement = `<h3>message</h3>`
+//     errordiv.innerHTML = innerElement;
 // }
 
-function addHerotoDom(hero){
+
+
+function addHerotoDom(hero) {
     let li = document.createElement('li');
-    li.innerHTML =`
+    li.innerHTML = `
     <img src="${hero.thumbnail.path+"."+hero.thumbnail.extension}" id = "poster"  >
     <h2 id="heroTitle"  data-id=${hero.id}>${hero.name}</h2>
     <button id="details" data-id="${hero.id}"> details </button>
@@ -38,92 +25,103 @@ function addHerotoDom(hero){
     superhero_list.append(li);
 }
 
-function renderList(){
-    if(heroes.length === 0){
-        let errormsg = 'Please input some valid hero name'
-        errordiv.innerHTML =errormsg ;
+function renderList() {
+    if (heroes.length === 0) {
+        let errormsg = `Please input some valid hero name`;
+
 
     }
-    errordiv.innerHTML = '';
-    superhero_list.innerHTML='';
+    // errordiv.innerHTML = '';
+    superhero_list.innerHTML = '';
 
-    if(heroes.length>0){
-        for(i =0 ;i<heroes.length;i++){
+    if (heroes.length > 0) {
+        for (i = 0; i < heroes.length; i++) {
             addHerotoDom(heroes[i]);
-            console.log(heroes);
+            // console.log(heroes);
         }
     }
 
 }
 
-async function search(text){
-    let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`;
-    let response = await fetch(url);
-    let data1 = await response.json();
-    heroes = await data1.data.results;
-    
-    renderList();
+async function search(text) {
+    if (text.length != 0) {
+        let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`;
+        let response = await fetch(url);
+        let data1 = await response.json();
+        heroes = data1.data.results;
 
-    
+        renderList();
+    } else {
+        renderList();
+    }
+
+
+
 
 }
 
-function addToFav(hero_id,hero_title){
-    for(i of fav){
-        if(hero_id == fav[i].id){
-            errormessage('${hero_title} already exists in favourites');
+function addToFav(hero_id, hero_title) {
+    console.log(hero_id);
+    for (i of fav) {
+        if (hero_id === i) {
+            errordiv.innerHTML = `${hero_title} already exists in favourites`;
+            console.log('test1');
             setTimeout(() => {
                 errordiv.innerHTML = '';
-            }, 3000);
+            }, 4000);
             return
         }
-        fav.push(hero_id);
-        errormessage('${hero_title}added to favourites');
-        setTimeout(() => {
-            errordiv.innerHTML = '';
-        }, 3000);
+
     }
-    
+    fav.push(hero_id);
+    console.log(hero_id);
+    errordiv.innerHTML = `${hero_title} added to favourites`;
+    console.log(errordiv.innerHTML);
+    setTimeout(() => {
+        errordiv.innerHTML = '';
+    }, 4000);
+
 
 }
 
-function detailed_info(){
+function detailed_info() {
 
 }
 
-function show_fav(){
+function show_fav() {
 
 }
 
-function handleInput(data){
+function handleInput(data) {
     let text = data.value;
     search(text);
 
 }
 
-function handleKeyandClick(e){
-    if(e.target.id == 'name_input'){
+function handleKeyandClick(e) {
+    if (e.target.id == 'name_input') {
         handleInput(e.target);
 
     }
-    if(e.target.id == 'favBtn' ){
-        addToFav(e.target.dataset.id,e.target.dataset.title);
+    if (e.target.id == 'favBtn') {
+        addToFav(e.target.dataset.id, e.target.dataset.title);
 
     }
-    if(e.target.id==='details'){
+    if (e.target.id === 'details') {
         let heroId = e.target.dataset.id;
         localStorage.setItem("heroId", JSON.stringify(heroId));
         window.open("hero_detail/detail.html");
 
     }
-    if(e.target.id=='favourite'){
-        localStorage.setItem("favHeros", JSON.stringify(favList));
+    if (e.target.id == 'favourite') {
+        localStorage.setItem("favHeros", JSON.stringify(fav));
         window.open("fav_hero/fav.html");
 
     }
-    
+
 
 }
 
-searchBtn.addEventListener('click',handleKeyandClick);
-name_input.addEventListener('keyup',handleKeyandClick);
+document.addEventListener('click', handleKeyandClick);
+document.addEventListener('keyup', handleKeyandClick);
+// favBtn.addEventListener('click',handleKeyandClick);
