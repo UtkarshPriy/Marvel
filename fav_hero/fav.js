7dd7bad3d6edb88582622afa4947b5fd20b5b0b2
UtkarshPriy <100868987+UtkarshPriy@users.noreply.github.com>
2023-09-09 15:36:21 +0530

    details

--- fav_hero/fav.js
+++ fav_hero/fav.js
@@ -69,7 +69,7 @@
             renderList();
 
         }
-        if (e.target.id === 'details') {
+        if (e.e.target.classList.contains('details')) {
             let heroId = e.target.dataset.id;
             localStorage.setItem("heroId", JSON.stringify(heroId));
             window.open("/hero_detail/detail.html");
